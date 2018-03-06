676c6bc2b11d08ac90817763e1d219acd9f8970f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-06 19:08:51 +0100

    directories

---
+++ test/signup_spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe("New user signup", function(){
+	describe("Using valid email, first, last", function(){
+		it("let's them in", function(){
+			assert(true);
+		});
+	});
+});
