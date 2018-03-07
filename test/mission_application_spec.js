7c866043174168892015fb3b063062f9878f0731
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-07 12:40:45 +0100

    membership app

---
+++ test/mission_application_spec.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var MembershipApplication = require("../lib/membership_application");
+
+describe("Applying for mission", function(){
+	describe("Using valid email, first, last, height, age, weight", function(){
+		it("let's them in");
+		it("reports valid height");
+		it("reports valid age");
+		it("reports valid weight");
+		it("reports valid name");
+	});
+});
