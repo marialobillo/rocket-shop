80971fff34bbd65063ec02885801c4e3689f76e3
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-07 13:29:25 +0100

    first test suite

--- test/mission_application_spec.js
+++ test/mission_application_spec.js
@@ -2,11 +2,36 @@ var assert = require('assert');
 var MembershipApplication = require("../lib/membership_application");
 
 describe("Applying for mission", function(){
+	var validApp;
+	before(function(){
+		validApp = new MembershipApplication({
+			first: "Test",
+			last: "User",
+			email: "test@test.com",
+			age: 30,
+			height: 66,
+			weight: 120,
+		});
+	});
 	describe("Using valid email, first, last, height, age, weight", function(){
+		it("is valid", function(){
+			assert(validApp.isValid(), "Not valid");
+		});
 		it("let's them in");
-		it("reports valid height");
-		it("reports valid age");
-		it("reports valid weight");
-		it("reports valid name");
+		it("reports a valid email", function(){
+			assert(validApp.emailIsValid());
+		});
+		it("reports valid height", function(){
+			assert(validApp.heightIsValid());
+		});
+		it("reports valid age", function(){
+			assert(validApp.ageIsValid());
+		});
+		it("reports valid weight", function(){
+			assert(validApp.weightIsValid());
+		});
+		it("reports valid name", function(){
+			assert(validApp.nameIsValid());
+		});
 	});
 });
